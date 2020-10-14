8d3197e926e8c2cb108de609a29a45c26a62ccfe
legoatoom <16699025+legoatoom@users.noreply.github.com>
2020-10-14 18:18:47 +0200

    Optimized Imports Last

--- src/main/java/com/github/legoatoom/enderbench/client/EnderBenchClient.java
+++ src/main/java/com/github/legoatoom/enderbench/client/EnderBenchClient.java
@@ -22,9 +22,7 @@ import com.github.legoatoom.enderbench.screen.ingame.EnderBenchScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
-import net.minecraft.client.options.KeyBinding;
 
 @Environment(EnvType.CLIENT)
 public class EnderBenchClient implements ClientModInitializer {
